408119d480eb5519cc8dc52e6ab1f219260e0522
Masaaki Komori <gmgateway@gmail.com>
2026-01-09 18:41:01 +0900

    Update modules and settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,12 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
+  // path aliases
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  // base path for deploying to a subdirectory
+  // base: './',
 })
